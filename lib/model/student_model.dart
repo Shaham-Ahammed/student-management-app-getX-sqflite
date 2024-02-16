8738a7228362c686662253e5db09707055052b9b
shaham ahammed p k <shahamahammed66@gmail.com>
2024-02-16 22:35:13 +0530

    added model

---
+++ lib/model/student_model.dart
@@ -0,0 +1,14 @@
+class StudentModel {
+  String name;
+  String age;
+  String phone;
+  String gender;
+  String images;
+  
+  StudentModel(
+      {required this.name,
+      required this.age,
+      required this.gender,
+      required this.images,
+      required this.phone});
+}
