ea3b9acb35e474a111dead3938f4a73d5f9e1aa0
shaham ahammed p k <shahamahammed66@gmail.com>
2024-02-18 11:11:30 +0530

    empty list handled

--- lib/screens/student list/student_list.dart
+++ lib/screens/student list/student_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:student_app_getx/controllers/student_controller.dart';
+import 'package:student_app_getx/screens/add%20student/functions/functions.dart';
 import 'package:student_app_getx/screens/student%20list/widgets/add_student_button.dart';
 import 'package:student_app_getx/screens/individual%20student/student_profile.dart';
 import 'widgets/app_bar.dart';
@@ -23,63 +25,63 @@ class StudentList extends StatelessWidget {
             ? const Center(
                 child: CircularProgressIndicator(),
               )
-            : Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const SearchBox(),
-                  Obx(() {
-                    return Expanded(
-                      child: ListView.builder(
-                        itemCount: controller.studentList.length,
-                        itemBuilder: (context, index) => GestureDetector(
-                          onTap: () => Get.to(
-                            fullscreenDialog: false,
-                            curve:Curves.easeInOutQuart,
-                            duration: const Duration(seconds:2),
-                            () => StudentProfile(
-                              id: controller.studentList[index].id!,
-                              name: controller.studentList[index].name,
-                              age: controller.studentList[index].age,
-                              images: controller.studentList[index].images,
-                              gender: controller.studentList[index].gender,
-                              phone: controller.studentList[index].phone)),
-                          child: Card(
-                            color: Colors.cyan[100],
-                            margin: const EdgeInsets.all(15),
-                            child: ListTile(
-                              title: CardTitle(
-                                  controller: controller, index: index),
-                              leading: CardImage(
-                                  controller: controller, index: index),
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  EditButton(
+            : controller.studentList.isEmpty
+                ? Center(
+                    child: Text(
+                    "no students added",
+                    style: TextStyle(fontFamily: GoogleFonts.aboreto().fontFamily,fontSize: 16,fontWeight: FontWeight.w600),
+                  ))
+                : Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const SearchBox(),
+                      Obx(() {
+                        return Expanded(
+                          child: ListView.builder(
+                            itemCount: controller.studentList.length,
+                            itemBuilder: (context, index) => GestureDetector(
+                              onTap: () => Get.to(
+                                  fullscreenDialog: false,
+                                  curve: Curves.easeInOutQuart,
+                                  duration: const Duration(seconds: 2),
+                                  () => StudentProfile(
+                                      id: controller.studentList[index].id!,
+                                      name: controller.studentList[index].name,
+                                      age: controller.studentList[index].age,
+                                      images:
+                                          controller.studentList[index].images,
+                                      gender:
+                                          controller.studentList[index].gender,
+                                      phone:
+                                          controller.studentList[index].phone)),
+                              child: Card(
+                                color: Colors.cyan[100],
+                                margin: const EdgeInsets.all(15),
+                                child: ListTile(
+                                  title: CardTitle(
                                       controller: controller, index: index),
-                                  deleteButton(controller, index, context),
-                                ],
+                                  leading: CardImage(
+                                      controller: controller, index: index),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      EditButton(
+                                          controller: controller, index: index),
+                                      deleteButton(controller, index, context),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    );
-                  }),
-                ],
-              );
+                        );
+                      }),
+                    ],
+                  );
       }),
       floatingActionButton: const StudentAddButton(),
     );
   }
-
-
- 
 }
-
-
-
-
-
-
